d24541707bab035a78a9d5950dbb3f37cfba5327
aaschuster <aaronjschuster@gmail.com>
2023-01-06 15:20:24 -0500

    build out initial switch with routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+//node imports
+
 import React from "react";
+import { useHistory, Route, Link, Switch } from "react-router-dom";
+
+//component imports
+
+import Form from "./Form"
+import Confirmation from "./Confirmation";
 
 const App = () => {
+  const history = useHistory();
+  const routeToPizza = () => history.push("/pizza");
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <h1>The Preppy Pepperoni Pizza Place</h1>
+      <Switch>
+        <Route path="/pizza">
+          <Form/>
+        </Route>
+        <Route path="/confirmation">
+          <Confirmation/>
+        </Route>
+        <Route path="/">
+        <button onClick={routeToPizza}>Click here for pizza!</button>
+        </Route>
+      </Switch>
+    </div>
   );
 };
+
 export default App;
---
+++ src/Confirmation.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Confirmation( props ) {
+    return (
+        <div>Confirmation</div>
+    )
+}
